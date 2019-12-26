24653a7d57eee57a8754d94682a23a041f0721ca
ZHanim <zilhan.be@gmail.com>
2019-12-26 13:42:35 +0100

    adding and playing with CRUD

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const port=3000;
 const app=express();
 
 
+const bodyParser = require('body-parser') 
+app.use(bodyParser.json()) //started to use body-parser
+app.use(bodyParser.urlencoded({ extended: true })) //??
+
+
 app.listen(port, () => {
     console.log("Server running on port 3000");
     console.log(team); //checking my data 
@@ -12,4 +17,31 @@ app.listen(port, () => {
 app.get('/',function(req, res){
     console.log('/home was requested'); 
     res.send(team)
+})
+
+app.get('/members',function(req, res){
+    console.log('/members was requested'); 
+    res.send(team.map(a=>a.member));
+})
+
+app.get('/member/:id',function(req, res){
+    const requestedId=req.url.split('/')[2];
+    console.log(`a certain member with id: ${requestedId} was requested`);
+    res.send(people.filter(member=>member.id==requestedId))
+})
+app.post('/member',function(req, res){
+    console.log(req.body);
+    people.push(req.body);
+    res.send(team);
+})
+
+app.put('/member/:id',function(req, res){
+    const tobeUpdatedId=req.url.split('/')[2];
+    people[tobeUpdatedId-1]=req.body;
+    res.send(team);
+})
+
+app.delete('/member/:id',function(req, res){
+    const tobeDeletedId=req.url.split('/')[2];
+    res.send(team.filter(member=>member.id!=tobeDeletedId)); //not use !==
 })
