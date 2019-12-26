8d88757ae6de62644661148843dfa22b612aed89
ZHanim <zilhan.be@gmail.com>
2019-12-26 13:34:51 +0100

    adding some mock data

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express=require('express');
-const data=require('/data')
+const team=require('./data')
 const port=3000;
 const app=express();
 
 
 app.listen(port, () => {
     console.log("Server running on port 3000");
-    console.log(data); //checking my data 
+    console.log(team); //checking my data 
    });
 
 app.get('/',function(req, res){
     console.log('/home was requested'); 
-    res.send(data)
+    res.send(team)
 })
--- data.js
+++ data.js
@@ -1,2 +1,7 @@
-const data=["something"]
-module.exports=data;
+const team=[
+    {"id":1, "member": "Hanim"},
+    {"id":2, "member": "David"},
+    {"id":3, "member": "Soroush"},
+    {"id":4, "member": "Andy"}
+]
+module.exports=team;
