0860fdb433e2a7e1f3cc06cc8be56594907a1d2f
Zeynep <zeynep@Zeyneps-MacBook-Pro.local>
2020-01-06 13:09:47 +0100

    data updated

---
+++ modal/tasks.js
@@ -0,0 +1,7 @@
+const tasks=[
+    {"id":1, "task": "Post newsletter to facebook", "by": "Hanim", "done": false},
+    {"id":2, "task": "Post happy new year on Twitter" , "by": "Sorush", "done": false},
+    {"id":3, "task": "Post React assigmnet", "by": "Andy", "done": false},
+    {"id":4, "task": "Announce company dinner on facebook", "by": "Keshia", "done": false}
+]
+module.exports=tasks;
