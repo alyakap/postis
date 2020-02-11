474996dbb16070301678a7e45325cd779f1941ee
HanimK <zilhan.be@gmail.com>
2020-02-11 16:36:55 +0100

    require dotenv into app.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 //requiring express and middlewares
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const mainRouter = require('./controller')
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mainRouter = require("./controller");
 
 //start server
 const port = 4567;
 const app = express();
 app.listen(port, () => {
-    console.log(`Server is listening on port:${port}...`);
-})
+  console.log(`Server is listening on port:${port}...`);
+});
 
 //setting up middlewares
 
 // add logs on the server's terminal
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // add cors support
 app.use(cors());
@@ -26,6 +26,4 @@ app.use(express.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(express.urlencoded({ extended: true }));
 
-
-mainRouter(app)  // require('./controller')(app) 
-
+mainRouter(app); // require('./controller')(app)
