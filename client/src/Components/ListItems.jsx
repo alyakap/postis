8e5680d82e654c262891aa4d617fe0a0a5b5865e
HanimK <zilhan.be@gmail.com>
2020-02-14 15:51:27 +0100

    creating home page

---
+++ client/src/Components/ListItems.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PublicIcon from "@material-ui/icons/Public";
+import HomeIcon from "@material-ui/icons/Home";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <PublicIcon />
+      </ListItemIcon>
+      <ListItemText primary="Campaigns" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <FormatListBulletedIcon />
+      </ListItemIcon>
+      <ListItemText primary="Tasks" />
+    </ListItem>
+  </div>
+);
